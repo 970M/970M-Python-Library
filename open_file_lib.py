3b1b2444bb975d6427bdca8ed1f8ff0b4920f867
Guillaume Daguet <g.daguet@ateme.com>
2023-11-25 17:52:26 +0100

    Refactoring

--- open_file_lib.py
+++ open_file_lib.py
@@ -1,117 +1,14 @@
 """Module Lecture/Ecriture fichiers"""
 
+import argparse
 
 
-def tab_ligne_fichier(cf):
-    fe = open(cf, "r")
-    lignes = fe.readlines()
-    fe.close()
-    return lignes
+parser = argparse.ArgumentParser()
 
-#
-# OUVERTURE FICHIER CSV
-#
-#==========================================================================
-# Remplissage du dictionnaire de donnÃ©e par ouverture classique de fichier
-#==========================================================================
-def TableDataFromClassicalOpenFile(csvFile):
+parser.add_argument("host")
+parser.add_argument("-c", "--clean", action="store_true", required=False)
 
-    """ Remplissage du dictionnaire de donnÃ©e par ouverture classique de fichier """
+args = parser.parse_args()
+print(args)
 
-    csvFileOut=csvFile+'.done1'
-    dictKeys=[]
-    tableData=[]
-
-    with open(csvFileOut, "w") as filout:
-        with open(csvFile, "r") as filin:
-
-            ligne = filin.readline() # type str
-
-            while ligne != "":
-                ligne=ligne.rstrip("\n").split(";") # type list
-                if not dictKeys:
-                    dictKeys=ligne
-
-                else:
-                    ligneData={}
-                    for k in range(len(dictKeys)):
-
-                        ligneData[dictKeys[k]]=ligne[k] # type dict
-
-                    tableData.append(ligneData)
-
-                newLigne=list(ligne)
-                #printDebug(newLigne)
-                newLigne.append(str(datetime.date.today())+"\n")
-                filout.write(";".join(newLigne))
-                ligne = filin.readline()
-
-    #printDebug("tableData=",tableData)
-    #printDebug("type=",type(tableData))
-
-    return(tableData) # Liste de dictionnaires
-
-#===================================================================================
-#  Remplissage du dictionnaire de donnÃ©e par ouverture via DictReader de biblio CSV
-#===================================================================================
-def TableDataFromCsvDictReader(csvFile):
-
-    import csv
-
-    csvFileOut=csvFile+'.done2'
-
-    filin=open(csvFile, "r")
-    tableData=list(csv.DictReader(filin,delimiter=";"))
-
-    #printDebug("table=",tableData)
-    #printDebug("type=",type(tableData))
-
-    filout=open(csvFileOut, "w")
-    tableDataDone=[]
-    for i in range(len(tableData)):
-        myDico=tableData[i].copy()
-        myDico["DONE_ON"]=str(datetime.date.today())
-        tableDataDone.append(myDico)
-
-    w=csv.DictWriter(filout, tableDataDone[0].keys(),delimiter=";")
-    w.writeheader()
-    w.writerows(tableDataDone)
-
-    filin.close()
-    filout.close()
-
-    return(tableData) # Liste de disctionnaires
-
-#===================================================================================
-# Remplissage du dictionnaire de donnÃ©e par ouverture via Reader de biblio CSV
-#===================================================================================
-def TableDataFromCsvReader(csvFile):
-
-    import csv
-    csvFileOut=csvFile+'.done3'
-    filin=open(csvFile, "r")
-    tableData=list(csv.reader(filin,delimiter=";"))
-    filout=open(csvFileOut, "w")
-    w=csv.writer(filout,delimiter=";")
-    newLine=list(tableData[0])
-    newLine.append("DONE_ON")
-    w.writerow(newLine)
-    for i in range(1,len(tableData)):
-        newLine=list(tableData[i])
-        newLine.append(str(datetime.date.today()))
-        w.writerow(newLine)
-
-    filin.close()
-    filout.close()
-
-    return(tableData) # Liste de listes
-
-#===================================================================================
-# Remplissage du dictionnaire de donnÃ©e par ouverture via panda_read de biblio pands: TBC car gestion de dataframe
-#===================================================================================
-def TableDataFromPandaRead(csvFile):
-
-    import pandas
-    return pandas.read_csv(csvFile)
-    #printDebug(df)
-    #printDebug("type=",type(df))
+print("Configure host", args.host)
